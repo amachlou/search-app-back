fe7bad4281f9d07cfcd4da63293fe2fab9abafc4
amachlou <abderrahim.machlou@gmail.com>
2025-06-29 13:14:05 +0100

    initial commit

---
+++ src/test/java/com/amachlou/search_app_back/SearchAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amachlou.search_app_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchAppBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
