bd268b5b5daa12bc310cf68507cf1bb3eafa5c5a
amachlou <abderrahim.machlou@gmail.com>
2025-07-10 20:38:34 +0100

    adding auditing

--- src/main/java/com/amachlou/search_app_back/entities/Auditable.java
+++ src/main/java/com/amachlou/search_app_back/entities/Auditable.java
@@ -1,2 +1,33 @@
-package com.amachlou.search_app_back.entities;public class AuditableEntity {
+package com.amachlou.search_app_back.entities;
+
+import jakarta.persistence.Column;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+public class Auditable {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
